e391e23cc32cedd689de9056beec952f29b4f51e
Angelo Groß <angelo.gross@education-siemens.com>
2014-02-05 13:27:17 +0100

    initial commit, v1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Now this is a test for merging conflicts.
+# It is also a repetition of the git-commands
+# I alread learned.
+
+# Copyright Angelo Groß 2014-02-05
---
+++ Safari.c
@@ -0,0 +1,22 @@
+/* This time we shall simulate a Safari.
+ * There will be two functions.
+ * seeLion() and seeElephant().
+ * The user shall press "L" if he wants to see
+ * a lion or "E" for an elephant.
+ * Team one will make the lion function and
+ * Team two will make the elephant function.
+ * Both teams have to come up with a solution
+ * for the main function...*/
+
+#include <stdio.h>
+
+seeLion()
+
+seeElephant()
+
+
+int main(void)
+{
+
+	return 0;
+}
